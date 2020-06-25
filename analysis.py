fb19e787cc32b4c985af04b8c6b4f90fef31957e
yaronlev9 <62477626+yaronlev9@users.noreply.github.com>
2020-06-25 13:57:35 +0300

    questions 1 to 3

--- analysis.py
+++ analysis.py
@@ -15,41 +15,41 @@
 
 def question2():
   answerDiscount = 0.9
-  answerNoise = 0.2
+  answerNoise = 0.005
   return answerDiscount, answerNoise
 
 def question3a():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerDiscount = 0.12
+  answerNoise = 0.00001
+  answerLivingReward = 0.75
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerDiscount = 0.05
+  answerNoise = 0.0005
+  answerLivingReward = 0.9
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerDiscount = 0.2
+  answerNoise = 0.00001
+  answerLivingReward = 0.75
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
   answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerNoise = 0.1
+  answerLivingReward = 0.9
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerDiscount = 1
+  answerNoise = 0.01
+  answerLivingReward = 1
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
--- valueIterationAgents.py
+++ valueIterationAgents.py
@@ -10,63 +10,89 @@ import mdp, util
 
 from learningAgents import ValueEstimationAgent
 
+
 class ValueIterationAgent(ValueEstimationAgent):
-  """
-      * Please read learningAgents.py before reading this.*
+    """
+        * Please read learningAgents.py before reading this.*
 
-      A ValueIterationAgent takes a Markov decision process
-      (see mdp.py) on initialization and runs value iteration
-      for a given number of iterations using the supplied
-      discount factor.
-  """
-  def __init__(self, mdp, discount = 0.9, iterations = 100):
+        A ValueIterationAgent takes a Markov decision process
+        (see mdp.py) on initialization and runs value iteration
+        for a given number of iterations using the supplied
+        discount factor.
     """
-      Your value iteration agent should take an mdp on
-      construction, run the indicated number of iterations
-      and then act according to the resulting policy.
-    
-      Some useful mdp methods you will use:
-          mdp.getStates()
-          mdp.getPossibleActions(state)
-          mdp.getTransitionStatesAndProbs(state, action)
-          mdp.getReward(state, action, nextState)
-    """
-    self.mdp = mdp
-    self.discount = discount
-    self.iterations = iterations
-    self.values = util.Counter() # A Counter is a dict with default 0
-     
-    "*** YOUR CODE HERE ***"
-    
-  def getValue(self, state):
-    """
-      Return the value of the state (computed in __init__).
-    """
-    return self.values[state]
 
-  def getQValue(self, state, action):
-    """
-      The q-value of the state action pair
-      (after the indicated number of value iteration
-      passes).  Note that value iteration does not
-      necessarily create this quantity and you may have
-      to derive it on the fly.
-    """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    def __init__(self, mdp, discount=0.9, iterations=100):
+        """
+          Your value iteration agent should take an mdp on
+          construction, run the indicated number of iterations
+          and then act according to the resulting policy.
 
-  def getPolicy(self, state):
-    """
-      The policy is the best action in the given state
-      according to the values computed by value iteration.
-      You may break ties any way you see fit.  Note that if
-      there are no legal actions, which is the case at the
-      terminal state, you should return None.
-    """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+          Some useful mdp methods you will use:
+              mdp.getStates()
+              mdp.getPossibleActions(state)
+              mdp.getTransitionStatesAndProbs(state, action)
+              mdp.getReward(state, action, nextState)
+        """
+        self.mdp = mdp
+        self.discount = discount
+        self.iterations = iterations
+        self.values = util.Counter()  # A Counter is a dict with default 0
+        "*** YOUR CODE HERE ***"
+        states = self.mdp.getStates()
+        for i in range(iterations):
+            old_values = self.values.copy()
+            for state in states:
+                best_value = None
+                for action in self.mdp.getPossibleActions(state):
+                    value = self.getQValue(state, action)
+                    if best_value == None or best_value < value:
+                        best_value = value
+                if best_value == None:
+                    best_value = 0
+                old_values[state] = best_value
+            self.values = old_values
 
-  def getAction(self, state):
-    "Returns the policy at the state (no exploration)."
-    return self.getPolicy(state)
-  
+    def getValue(self, state):
+        """
+          Return the value of the state (computed in __init__).
+        """
+        return self.values[state]
+
+    def getQValue(self, state, action):
+        """
+          The q-value of the state action pair
+          (after the indicated number of value iteration
+          passes).  Note that value iteration does not
+          necessarily create this quantity and you may have
+          to derive it on the fly.
+        """
+        "*** YOUR CODE HERE ***"
+        value = 0
+        for next_state, prob in self.mdp.getTransitionStatesAndProbs(state, action):
+            value += prob * (self.mdp.getReward(state, action, next_state) + self.discount * self.getValue(next_state))
+        return value
+
+    def getPolicy(self, state):
+        """
+          The policy is the best action in the given state
+          according to the values computed by value iteration.
+          You may break ties any way you see fit.  Note that if
+          there are no legal actions, which is the case at the
+          terminal state, you should return None.
+        """
+        "*** YOUR CODE HERE ***"
+        possibleActions = self.mdp.getPossibleActions(state)
+        if not possibleActions:
+            return
+        best_value = None
+        best_action = None
+        for action in possibleActions:
+            value = self.getQValue(state, action)
+            if (not best_value) or value > best_value:
+                best_value = value
+                best_action = action
+        return best_action
+
+    def getAction(self, state):
+        "Returns the policy at the state (no exploration)."
+        return self.getPolicy(state)
